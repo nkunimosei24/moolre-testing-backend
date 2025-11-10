f893df4e8d67456193b252e3f1e5df234433027a
Nkunim Asaah Osei <oseinkunim@gmail.com>
2025-11-10 04:33:03 +0000

    modified cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 app.use(
     cors({
-        origin: 'http://localhost:5175',
+        origin: ['http://localhost:5175', 'https://moolre-test.netlify.app'],
         credentials: true
     })
 )
